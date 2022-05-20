42d0d71204b60a33fe732ccffd329ee7590d41ee
Aranza747 <aranzaromero@comunidad.unam.mx>
2022-05-20 17:45:13 -0500

    agregue todos los archivos

---
+++ dynamics/js/tareas.js
@@ -0,0 +1,29 @@
+
+
+const agregar = document.getElementById("agregar");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+const botones = document.getElementById("boton");
+const otra = document.getElementById("otra");
+const materia = document.getElementById("materia");
+
+
+
+ul.addEventListener("click", (evento)=>{ 
+    if(evento.target.className==='boton'){
+        evento.target.parentElement.outerHTML = '';
+    }
+});
+
+otra.addEventListener("click", (evento) =>{
+    if(evento.target.otra == true){
+        '<label class="materia">Escribe tu materia</label>'
+        '<input type="text"></input>'
+    }
+});
+
+agregar.addEventListener("click", (evento)=>{
+    ul.innerHTML += '<li id="">'+input.value +'<button class="boton"><br>Añadir</button> </li>';
+});
+
+
